611764b3be6e03ffb4c5adb77f6c55677617ef2e
GotadeOrvalho <adrianeferreirap@gmail.com>
2024-07-25 05:10:31 -0300

    Good solution

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,54 +1,59 @@
 package aplication;
 
+
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Room number: ");
-		Integer number = sc.nextInt();
-		System.out.println("Chek-in date (DD/MM/YYYY): ");
-		sc.nextLine();
-		LocalDate in = LocalDate.parse(sc.nextLine(), dtf);
-		System.out.println("Chek-out date (DD/MM/YYYY): ");
-		LocalDate out = LocalDate.parse(sc.nextLine(), dtf);
-
-		if (!out.isAfter(in)) {
-			System.out.println("Error in reservation: Chek-out date must be after chek-in date.");
-		} else {
+		try {
+			System.out.println("Room number: ");
+			Integer number = sc.nextInt();
+			System.out.println("Chek-in date (DD/MM/YYYY): ");
+			sc.nextLine();
+			LocalDate in = LocalDate.parse(sc.nextLine(), dtf);
+			System.out.println("Chek-out date (DD/MM/YYYY): ");
+			LocalDate out = LocalDate.parse(sc.nextLine(), dtf);
+	
 			Reservation a = new Reservation(number, in, out);
 			System.out.println("Reservation: " + a);
-
+	
 			System.out.println(" ");
-
+	
 			System.out.println("Enter data to update the reservations: ");
 			System.out.println("Chek-in date (DD/MM/YYYY): ");
-
+	
 			in = LocalDate.parse(sc.nextLine(), dtf);
 			System.out.println("Chek-out date (DD/MM/YYYY): ");
 			out = LocalDate.parse(sc.nextLine(), dtf);
-
-			String error = a.updateDates(in, out);
-			if (error != null) {
-
-				System.out.println("Error in reservation: " + error);
-			} else {
-
-				System.out.println("Reservation: " + a);
-			}
-
+	
+			a.updateDates(in, out);
+		
+		System.out.println("Reservation: " + a);
+		} 
+		catch (DateTimeParseException e) {
+			System.out.println("Invalid date format.");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error.");
 		}
 
+	
 		sc.close();
 
 	}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,73 +4,72 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.exceptions.DomainException;
 
 public class Reservation {
 	private Integer roomNumber;
 	private LocalDate chekIn;
 	private LocalDate chekOut;
-	
+
 	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
+
 	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
+
 	public LocalDate getChekIn() {
 		return chekIn;
 	}
-	
+
 	public LocalDate getChekOut() {
 		return chekOut;
 	}
-	
+
 	public Reservation() {
-		
+
 	}
-	
+
 	public Reservation(Integer roomNumber, LocalDate chekIn, LocalDate chekOut) {
-	
+		if (!chekOut.isAfter(chekIn)) {
+
+			throw new DomainException("Chek-out date must be after chek-in date.");
+		}
 		this.roomNumber = roomNumber;
 		this.chekIn = chekIn;
 		this.chekOut = chekOut;
 	}
-	
+
 	public long duration() {
 		Duration c = Duration.between(chekIn.atStartOfDay(), chekOut.atStartOfDay());
-		
+
 		return c.toDays();
-		
+
 	}
-	
-	public String updateDates(LocalDate chekIn, LocalDate chekOut) {
+
+	public void updateDates(LocalDate chekIn, LocalDate chekOut) {
 		LocalDate now = LocalDate.now();
 
 		if (chekIn.isBefore(now) || chekOut.isBefore(now)) {
-			
-			return "Reservations dates for update must be future dates.";
-		} 
-		if (!chekOut.isAfter(chekIn)) {
-			
-			return "Chek-out date must be after chek-in date.";
+
+			throw new DomainException("Reservations dates for update must be future dates.");
 		}
-			
+		if (!chekOut.isAfter(chekIn)) {
+
+			throw new DomainException("Chek-out date must be after chek-in date.");
+		}
+
 		this.chekIn = chekIn;
 		this.chekOut = chekOut;
-		return null;		
+
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", chek-in: "
-				+ dtf.format(this.chekIn)
-				+ ", chek-out: "
-				+ dtf.format(this.chekOut)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", chek-in: " + dtf.format(this.chekIn) + ", chek-out: "
+				+ dtf.format(this.chekOut) + ", " + duration() + " nights";
 	}
 }
